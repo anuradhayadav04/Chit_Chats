44a908a4219414ced5d70adeda61710741fd08c6
Team  Astra <yadavanuradha2004@gmail.com>
2025-07-12 11:35:50 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,6 +27,10 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.get("/", (req, res) => {
+  res.send("ChitChat Backend is Live 🚀");
+});
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
