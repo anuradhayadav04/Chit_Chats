01dabad42f9c5d8813ad6a82d39444163e739953
Team  Astra <yadavanuradha2004@gmail.com>
2025-07-19 10:01:26 +0530

    deployment added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chit-chats-1.onrender.com/api" : "/api",
   withCredentials: true,
 });
